3b259fe843c75718a4cb9b1e398871f991868c1c
portsungbin <sd61224@gmail.com>
2025-02-10 14:50:44 +0900

    commit

---
+++ src/day05/formatEx/FormatEx.java
@@ -0,0 +1,15 @@
+package day05.formatEx;
+
+/*
+* format(형식) 클래스는 숫자 또는 날짜를 원하는 형태로 문자열로 변환해 주는 기능
+* java.text 패키지 제공
+* - DecimalFormat : 숫자를 형식화된 문자열로 변환
+* - SimpleDateFormat : 날짜를 형식환된 문자열로 변환
+*
+* */
+
+public class FormatEx {
+    public static void main(String[] args) {
+
+    }
+}
