c1235226d502811a39235f795a748302d4e1e336
portsungbin <sd61224@gmail.com>
2025-02-11 12:53:39 +0900

    commit

---
+++ src/day06/gennericEx/lamdaEx/StreamEx.java
@@ -0,0 +1,18 @@
+package day06.gennericEx.lamdaEx;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamEx {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("functional", "programming", "with", "generice", "in", "java");
+
+        List<String> filteredWords = words.stream()
+                .filter(word -> word.length() > 4)
+                .map(word -> word.toUpperCase())
+                .collect(Collectors.toList());
+
+        System.out.println(filteredWords);
+    }
+}
